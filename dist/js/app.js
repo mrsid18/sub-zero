cdda4025e842e627d56bab4f52d808b0f497030f
Siddhartha Mehta <siddharthamehta18@gmail.com>
2021-07-30 11:40:49 +0530

    added mobile navigation

---
+++ dist/js/app.js
@@ -0,0 +1,8 @@
+function togglemenu(){
+    let menu = document.getElementById("mobile-menu");
+    if (menu.style.display == "none"){
+        menu.style.display = "block"
+    }else{
+        menu.style.display = "none";
+    }
+}
